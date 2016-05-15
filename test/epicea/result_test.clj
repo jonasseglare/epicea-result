43f4cda8dfa7974f482452582ade90d2ae85bc7b
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-05-15 15:16:25 +0200

    good

--- test/epicea/result_test.clj
+++ test/epicea/result_test.clj
@@ -9,4 +9,6 @@
     (is (not (result? [])))
     (is (failure? (make-failure 3)))
     (is (not (failure? (make-success 3))))
-    (is (= 3 (get-value (make-success 3))))))
+    (is (= 3 (get-value (make-success 3))))
+    (is (success? (make-success 34234)))
+    (is (not (success? (make-failure :baja))))))
